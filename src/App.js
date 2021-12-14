2f1a30a7ac91230020cb6c922ad25a2ab226f290
Milton Clark <kingcoder247@gmail.com>
2021-12-13 20:42:56 -0500

    Add - handleUserInput and handleOnSubmit, setup fetch and search results page, Css for page layout

--- src/App.js
+++ src/App.js
@@ -1,56 +1,21 @@
 
 import { Component } from 'react';
 import './App.css';
+import Home from './components/Home';
 
 class App extends Component{
   constructor(){
     super()
-    this.state = {
-      searchInputValue: "",
-      fetchData: [],
-
-    }
   }
 
-  handleOnSubmit = (e) => {
-    e.preventDefault();
+ 
 
-
-  }
-
-  handleUserInput = (e) => {
-    this.setState({
-      [this.searchInputValue]: e.target.value
-    })
-  }
+ 
 
   render(){
     return(
       <div className="App">
-        <div id="navbar-container">
-          NavBar
-        </div>
-
-        <div id="search-container">
-          <form id="search-form">
-            <input 
-              id="search-box" 
-              type="text" 
-              placeholder="Search..." 
-              value={this.searchInputValue}
-              onInput={this.handleUserInput}
-            />
-            <button id="search-button" onClick={this.handleOnSubmit}>Search</button>
-          </form>
-        </div>
-
-        <div id="display-container">
-          <div id="search-results">
-            <p>
-              No Search Results Yet! Please submit a search above!
-            </p>
-          </div>
-        </div>
+        <Home />
 
     
       </div>
