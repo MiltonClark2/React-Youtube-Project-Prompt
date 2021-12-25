bf79f2a7f53fe9c240081ee5b13efe5a0588889f
Milton Clark <kingcoder247@gmail.com>
2021-12-25 15:02:09 -0500

    Updated files for deployment

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link, Route, Switch } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import '../App.css';
 import HomePage from './Home';
 import AboutUsPage from './About';
